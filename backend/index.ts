ab4474dee8beabe0751b4c021d5641072ce5fd07
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-08-04 21:35:54 +1000

    Support uploading images with multer

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,13 @@
 import express, { Response, Express, Request } from 'express'
 import { Client } from 'pg'
+import multer from 'multer'
 
 import { dbMiddleware } from './middleware/db'
 
 const app: Express = express()
 app.use(dbMiddleware)
+
+const upload = multer({ dest: 'public' })
 
 declare global {
   namespace Express {
@@ -15,7 +18,8 @@ declare global {
   }
 }
 
-app.post('/images', async (req: Request, res: Response) => {
+app.post('/images', upload.array('images'), async (req: Request, res: Response) => {
+  // access files in req.files
   res.json({ msg: 'ok' })
 })
 
@@ -25,6 +29,10 @@ app.get('/images', async (req: Request, res: Response) => {
   console.log('rows', query.rows)
   res.send(`
     <h3>Upload images</h3>
+    <form action="/images" method="post" enctype="multipart/form-data">
+      <input type="file" name="images">
+      <button type="submit">Submit</button>
+    </form>
     <div></div>
   `)
 })
